aa55c8bafbc227fbba29ffca673a146d0684eae3
smithwebtek <brad@smithwebtek.com>
2018-12-19 12:49:48 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-comment.create(content: 'wow')
-comment.create(content: 'yay')
+Comment.create(content: 'wow')
+Comment.create(content: 'yay')
